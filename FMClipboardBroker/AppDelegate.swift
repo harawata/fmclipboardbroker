5f8c995ea274e6a2f102784391c8477b23aca512
Iwao AVE! <harawata@gmail.com>
2016-05-28 17:04:57 +0900

    Initial Commit

---
+++ FMClipboardBroker/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  FMClipboardBroker
+//
+//  Created by Iwao AVE! on 2016/05/28.
+//  Copyright © 2016年 Iwao AVE!. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ FMClipboardBrokerTests/FMClipboardBrokerTests.swift
@@ -0,0 +1,36 @@
+//
+//  FMClipboardBrokerTests.swift
+//  FMClipboardBrokerTests
+//
+//  Created by Iwao AVE! on 2016/05/28.
+//  Copyright © 2016年 Iwao AVE!. All rights reserved.
+//
+
+import XCTest
+@testable import FMClipboardBroker
+
+class FMClipboardBrokerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
